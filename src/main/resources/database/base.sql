fc4f627e9741799d9f11c65faf14e990b4ef1d85
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-01-15 18:19:18 +0300

    Update base.sql

--- src/main/resources/database/base.sql
+++ src/main/resources/database/base.sql
@@ -1,3 +1,5 @@
+create extension if not exists btree_gist;
+
 create table patients(
     id serial primary key,
     nom varchar(100) not null
@@ -12,13 +14,15 @@ create table patient_symptomes(
     id serial primary key,
     id_patient int references patients(id),
     id_symptome int references symptomes(id),
-    intensite int check (intensite >= 0 and intensite <= 10)
+    intensite int check (intensite >= 0 and intensite <= 10),
+    unique (id_patient, id_symptome, intensite)
 );
 
 create table parametres(
     id serial primary key,
     id_symptome int references symptomes(id),
-    intensite int check (intensite >= 0 and intensite <= 10)
+    plage_intensite int4,
+    EXCLUDE USING gist (plage_intensite WITH &&, id_symptome WITH =)
 );
 
 create table maladies(
@@ -29,7 +33,8 @@ create table maladies(
 create table maladie_parametres(
     id serial primary key,
     id_maladie int references maladies(id),
-    id_parametre int references parametres(id)
+    id_parametre int references parametres(id),
+    unique (id_maladie, id_parametre)
 );
 
 create table medicaments(
@@ -41,5 +46,6 @@ create table medicament_parametres(
     id serial primary key,
     id_medicament int references medicaments(id),
     id_parametre int references parametres(id),
-    effet int check (effet >= 0 and effet <= 10)
+    effet int check (effet >= 0 and effet <= 10),
+    unique (id_medicament, id_parametre, effet)
 );
