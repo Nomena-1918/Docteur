cf31f1e395eca875db0da573752d30915f4acb87
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-01-17 08:22:56 +0300

    Create MedicamentRepository.java

---
+++ src/main/java/org/docteur/docteur/repositories/MedicamentRepository.java
@@ -0,0 +1,9 @@
+package org.docteur.docteur.repositories;
+
+import org.docteur.docteur.models.Medicament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicamentRepository extends JpaRepository<Medicament, Long> {
+}
